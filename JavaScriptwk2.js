346520ad902edb3974d73f805c63b65121fa0e23
Kelby Estes-Nelson <kelbyalese417@gmail.com>
2021-11-21 14:12:29 -0600

    Week3Homework

---
+++ JavaScriptwk2.js
@@ -0,0 +1,16 @@
+let pizzaPlace = "Tonys";
+let numberOfToppings = 11
+;
+
+//console.log("I love" + pizzaPlace + "because they have" + numberOfToppings + "toppings!");
+console.log(`I love ${pizzaPlace} because they have ${numberOfToppings} toppings!`);
+console.log(pizzaPlace, typeof pizzaPlace);
+console.log(numberOfToppings, typeof numberOfToppings);
+
+
+if (numberOfToppings < 10) {
+  console.log("A whole lot of pizza!");
+} else  {
+  console.log("Quality, not Quantity");
+}
+
